9a804044a3035e11672b022f40a2fa9e21b53602
João Pereira <pereiraffjoao1993@gmail.com>
2020-09-23 22:58:24 +0100

    Added support to save images on a season folder. Make persons in charge and channels configurable.

--- football-annoucer/index.js
+++ football-annoucer/index.js
@@ -27,20 +27,21 @@ const logger = winston.createLogger({
 // Create an instance of a Discord client
 const client = new discord.Client();
 
-// creating folder to put matches generated images: path starts in the root directory discord-bots
-var dir = path.join(module.path, '/images/games/');
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir, {recursive: true}, err => {
-        logger.error(`Something went wrong whn trying to create games directory. ${err}`)
-    });
-}
-
 // authentication tokens and settings
 var auth = require('./auth.json');
 var settings = require('./settings.json');
 
-async function print_image(teamsImagesPath, homeTeam, awayTeam, callback) {
-    var imagePath = path.join(module.path, `/images/games/${homeTeam}vs${awayTeam}.png`);
+async function print_image(teamsImagesPath, homeTeam, awayTeam, season, callback) {
+    var dir = path.join(module.path, '/images/games/' + season + '/');
+
+    // creating folder to put matches generated images
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir, {recursive: true}, err => {
+            logger.error(`Something went wrong whn trying to create games directory. ${err}`)
+        });
+    }
+
+    var imagePath = path.join(dir, `${homeTeam}vs${awayTeam}.png`);
     // cant have special characters or whitespaces
     var imageAttachmentName = `${homeTeam}vs${awayTeam}`.replace(/[^a-zA-Z ]|\s/g, "") + ".png";
     await mergeImg(
@@ -50,7 +51,7 @@ async function print_image(teamsImagesPath, homeTeam, awayTeam, callback) {
         ]).then((img) => {
             // Save image as file
             img.write(imagePath, () => {  
-                logger.debug(`Generated image for game ${homeTeam} vs ${awayTeam} at ${path.join(module.path, '/images/games/')}`);
+                logger.debug(`Generated image for game ${homeTeam} vs ${awayTeam} at ${imagePath}`);
                 callback(imagePath, imageAttachmentName)
             });
         });
@@ -66,7 +67,9 @@ function GetSimplifiedDate(date) {
 function scheduleGame(game) {
     var gameScheduleDate = new Date(Date.parse(String(game.utcDate)));
     var notificationScheduleDate = new Date(gameScheduleDate);
-    notificationScheduleDate.setMinutes(notificationScheduleDate.getMinutes() - 15)
+    notificationScheduleDate = new Date()
+    notificationScheduleDate.setSeconds(notificationScheduleDate.getSeconds() + 10)
+    //notificationScheduleDate.setMinutes(notificationScheduleDate.getMinutes() - 15)
 
     var hours = ("0" + notificationScheduleDate.getHours()).slice(-2);
     var minutes = ("0" + notificationScheduleDate.getMinutes()).slice(-2);
@@ -74,17 +77,31 @@ function scheduleGame(game) {
 
     var homeTeam = game.homeTeam.name;
     var awayTeam = game.awayTeam.name;
+    var season = game.season.id;
 
     logger.info(`Match betweeen ${homeTeam} and ${awayTeam} will be announced at ${hours}:${minutes}:${seconds}`);
     schedule.scheduleJob(notificationScheduleDate, function(){
 
-        print_image(path.join(module.path, '/images/teams/'), homeTeam, awayTeam, function(imagePath, imageAttachmentName) {
+        print_image(path.join(module.path, '/images/teams/'), homeTeam, awayTeam, season, function(imagePath, imageAttachmentName) {
             var hours = ("0" + gameScheduleDate.getHours()).slice(-2);
             var minutes = ("0" + gameScheduleDate.getMinutes()).slice(-2);
             var attachments = [
                 new discord.MessageAttachment(path.join(module.path, '/images/Liga Nos.png'), 'LigaNos.png'),
                 new discord.MessageAttachment(imagePath, imageAttachmentName)
             ];
+
+            var personsInChargeResultStr = "";
+            var personsInCharge = settings.persons_in_charge.split(",");
+            if (personsInCharge.length > 0) {
+                personsInCharge.forEach(function(element, i) {
+                    if (i != 0 && i < personsInCharge.length - 1) {
+                        personsInChargeResultStr += ", ";
+                    } else if (i != 0 && i == personsInCharge.length - 1) {
+                        personsInChargeResultStr += " ou ";
+                    }
+                    personsInChargeResultStr += `${client.users.cache.get(element)}`;
+                });
+            }
             const embedMessage = new discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`Liga NOS - ${GetSimplifiedDate(gameScheduleDate)}`)
@@ -95,20 +112,32 @@ function scheduleGame(game) {
                     { name: 'Próximo Jogo', value: `${homeTeam} vs ${awayTeam}` },
                     { name: 'Horário', value: `${hours}:${minutes}h`},
                     { name: 'Canal', value: 'O Tasco :soccer: :beer: :hotdog:' },
-                    { name: 'Bilhetes com', value: `${client.users.cache.get("165606532325572609")} e ${client.users.cache.get(settings.streamer_id)}` },
+                    { name: 'Bilhetes com', value: personsInChargeResultStr },
                 )
                 .setImage(`attachment://${imageAttachmentName}`)
                 .attachFiles(attachments);
+
+            if (personsInCharge.length > 0) {
+                personsInCharge.forEach(element => {
+                    const personsInChargeMessage = new discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`Liga NOS - ${GetSimplifiedDate(gameScheduleDate)}`)
+                        .setAuthor('Liga NOS', 'attachment://LigaNos.png')
+                        .setDescription('Hey, o próximo jogo da Liga NOS está prestes a começar!')
+                        .setThumbnail('attachment://LigaNos.png')
+                        .addFields(
+                            { name: 'Próximo Jogo', value: `${homeTeam} vs ${awayTeam}` },
+                            { name: 'Horário', value: `${hours}:${minutes}h`},
+                        )
+                        .setImage(`attachment://${imageAttachmentName}`)
+                        .attachFiles(attachments);
+
+                    client.users.cache.get(element).send({embed: personsInChargeMessage});
+                    });
+            }
             
-            client.channels.cache.get(settings.channel_notifications).send({embed: embedMessage});
-            const streamerMessage = new discord.MessageEmbed()
-                .setAuthor('Liga NOS', 'attachment://LigaNos.png')
-                .setDescription('Está na hora de abrir o tasco! :smile:')
-                .addFields(
-                    { name: 'Próximo Jogo', value: `${homeTeam} vs ${awayTeam}` },
-                    { name: 'Horário', value: `${hours}:${minutes}h`}
-                );
-            client.users.cache.get(settings.streamer_id).send({embed:streamerMessage});
+            if (settings.announcements_channel.trim())
+                client.channels.cache.get(settings.announcements_channel).send({embed: embedMessage});
         });
     });
 }
@@ -124,7 +153,7 @@ function processResquestResponse(error, response, body) {
         }
     }
     else {
-        logger.debug("Something went wrong during daily matches request.")
+        logger.debug("Something went wrong during daily matches request.");
     }
 }
 
@@ -135,6 +164,7 @@ function startDailyScheduleTask() {
         if (settings.is_daily_job_active) {
             var todayDate = new Date()
             todayDate = GetSimplifiedDate(todayDate)
+            todayDate = "2020-09-19"
             var finalUrl = settings.football_data_api + `matches?dateFrom=${todayDate}&dateTo=${todayDate}&competitions=${settings.football_data_competitions}`
     
             logger.debug("Fetching daily matches request: " + finalUrl)
@@ -159,33 +189,39 @@ client.on('ready', () => {
 
 // Create an event listener for messages
 client.on('message', message => {
-    let args = message.content.substring(settings.bot_prefix_command.length).split(" ");
-    switch(args[0]) {
-        case 'help':
-            const embed = 
-            {
-                title: 'Liga NOS Fetcher Commands',
-                fields: [{
-                        name: 'Prefix (+)',
-                        value: `Example: +help`
-                },{
-                        name: 'Enable',
-                        value: `enable - Enable notifications for the next Liga NOS matches to the announcements channel.`
-                }, {
-                        name: 'Disable',
-                        value: `disable - Disables notifications for the next Liga NOS matches on the announcements channel.`
-                }, {
-                        name: 'Discord.js',
-                        value: `v${discord.version}`
-                }]
-            }
-            message.channel.send('', {embed:embed});
-            break
-        case 'enable':
-            settings.is_daily_job_active = true;
-            break;
-        case 'disable':
-            settings.is_daily_job_active = false;
+    let prefix_on_message = message.content.substring(0, settings.bot_prefix_command.length)
+    let check_prefix = prefix_on_message === settings.bot_prefix_command;
+
+    if (check_prefix)
+    {
+        let args = message.content.substring(settings.bot_prefix_command.length).split(" ");
+        switch(args[0]) {
+            case 'help':
+                const embed = 
+                {
+                    title: 'Liga NOS Fetcher Commands',
+                    fields: [{
+                            name: 'Prefix (+)',
+                            value: `Example: +help`
+                    },{
+                            name: 'Enable',
+                            value: `enable - Enable notifications for the next Liga NOS matches to the announcements channel.`
+                    }, {
+                            name: 'Disable',
+                            value: `disable - Disables notifications for the next Liga NOS matches on the announcements channel.`
+                    }, {
+                            name: 'Discord.js',
+                            value: `v${discord.version}`
+                    }]
+                }
+                message.channel.send('', {embed:embed});
+                break
+            case 'enable':
+                settings.is_daily_job_active = true;
+                break;
+            case 'disable':
+                settings.is_daily_job_active = false;
+        }
     }
 });
 
